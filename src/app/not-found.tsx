e5fe910bc970bf830e9a193f7e31098d7994b73e
shema kaitakirwa <skaitakirwa@gmail.com>
2025-11-28 08:12:21 +0200

    align not found screen with error screen

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import { ROUTES } from "@/common/constants";
+import styles from "./not-found.module.scss";
+
 export default function NotFound() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={styles.container}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page or product you&apos;re looking for doesn&apos;t exist.</p>
+      <Link href={ROUTES.HOME} className={styles.button}>
+        Go back home
+      </Link>
+    </div>
+  );
 }
